e55e1bd2a5b585c5b8b3221d7fc2c033c4ee2a29
Abhas Kumar <abhas1102@gmail.com>
2021-07-21 14:17:18 +0530

    Set the questions model and inserted all the questions that is needed to display.

--- app/src/main/java/com/example/quizgame/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.quizgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        buttonStart.setOnClickListener {
+            if(txtInput.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT).show()
+            } else{
+               val intent = Intent(this,QuestionsActivity::class.java )
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/quizgame/Questions.kt
@@ -0,0 +1,12 @@
+package com.example.quizgame
+
+data class Questions (
+        val id: Int,
+        val questions: String,
+        val image: Int,
+        val optionOne: String,
+        val optionTwo: String,
+        val optionThree: String,
+        val optionFour: String,
+        val correctAnswer: Int
+        )
