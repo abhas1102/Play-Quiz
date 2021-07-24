a14d10bad1fa91d0d4f0df1ae0c9692c367cc8e3
Abhas Kumar <abhas1102@gmail.com>
2021-07-25 01:22:10 +0530

    1. Made the options textview clickable 2. Provided some UI shape to textview similar to Buttons 3. Changed the view for selected option.

--- app/src/main/java/com/example/quizgame/QuestionsActivity.kt
+++ app/src/main/java/com/example/quizgame/QuestionsActivity.kt
@@ -1,17 +1,32 @@
 package com.example.quizgame
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_questions.*
+import org.w3c.dom.Text
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    private var mCurrentPosition:Int = 1
+    private var mQuestionsList:ArrayList<Questions>?=null
+    private var mSelectedOptionPosition:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Question size", "${questionsList.size}")
+        mQuestionsList = Constants.getQuestions()
+        setQuestion()
+
+        text_optionOne.setOnClickListener(this)
+        text_optionTwo.setOnClickListener(this)
+        text_optionThree.setOnClickListener(this)
+        text_optionFour.setOnClickListener(this)
+        // Log.i("Question size", "${questionsList.size}")
 
       /*  for(i in questionsList.size-1 downTo 0){
 
@@ -27,11 +42,17 @@ class QuestionsActivity : AppCompatActivity() {
 
         } */  // Lots of errors are there when I tried to run loop on Arraylist.
 
-        val currentPosition = 1
-        val question:Questions? = questionsList[currentPosition-1]
 
-        progressBar.progress = currentPosition
-        numericalProgress.text = "$currentPosition"+"/"+progressBar.max
+
+    }
+
+    private fun setQuestion(){
+        mCurrentPosition = 1
+        val question= mQuestionsList!![mCurrentPosition-1]
+        defaultOptionsView()
+
+        progressBar.progress = mCurrentPosition
+        numericalProgress.text = "$mCurrentPosition"+"/"+progressBar.max
 
         question_text.text = question!!.questions
         question_image.setImageResource(question.image)
@@ -39,6 +60,47 @@ class QuestionsActivity : AppCompatActivity() {
         text_optionTwo.text = question.optionTwo
         text_optionThree.text = question.optionThree
         text_optionFour.text = question.optionFour
+
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        options.add(0,text_optionOne)
+        options.add(1,text_optionTwo)
+        options.add(2,text_optionThree)
+        options.add(3,text_optionFour)
+
+        for(option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.text_optionOne -> {
+                selectedOptionView(text_optionOne,1)
+            }
+            R.id.text_optionTwo-> {
+                selectedOptionView(text_optionTwo,2)
+            }
+            R.id.text_optionThree -> {
+                selectedOptionView(text_optionThree,3)
+            }
+            R.id.text_optionFour -> {
+                selectedOptionView(text_optionFour,4)
+            }
+        }
+    }
+
+    private fun selectedOptionView(tv:TextView, selectedOptionNum:Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface,Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this,R.drawable.selected_option_border_bg)
+
 
     }
 }
